f6f79fc9e1ffaf894cef0fbb21b68e52d371428c
Nator265 <kelvinzimba2322000@gmail.com>
2024-08-27 15:02:33 +0100

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
       <div className="h-[80vh] w-[100wh] md:hidden overflow-hidden">
         <div className="border h-[80vh] w-[100wh] overflow-hidden relative top-0 z-1">
-          {/* <Image
+          <Image
           src="/burger.png"
           alt="Juicy Burger"
           layout="responsive"
@@ -19,7 +19,7 @@ export default function Home() {
           width={200}
           objectFit="cover"
           quality={100}
-          />  */}
+          /> 
         </div>
       </div>
 
@@ -35,7 +35,7 @@ export default function Home() {
           transition={{ duration: 1, ease: "easeOut" }}
         >
         {/* Content can go here */}
-      </motion.div>
+        </motion.div>
       </div>
     </main>
   );
