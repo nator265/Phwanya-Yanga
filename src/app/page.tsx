fd8fff5f6a7b88d1caa019e401b58c2e7149db5d
Nator265 <kelvinzimba2322000@gmail.com>
2024-08-27 14:55:01 +0100

    image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,28 @@ export default function Home() {
   return (
     <main className="">
       <Header />
-      <div className="border h-[80vh] md:hidden">
-        <Image src="/burger-front2.jpg" width={500} height={100} alt="Juicy-Burger" />
+
+      <div className="h-[80vh] w-[100wh] md:hidden overflow-hidden">
+        <div className="border h-[80vh] w-[100wh] overflow-hidden relative top-0 z-1">
+          <Image
+          src="/burger.png"
+          alt="Juicy Burger"
+          layout="responsive"
+          height={200}
+          width={200}
+          objectFit="cover"
+          quality={100}
+          /> 
+        </div>
       </div>
 
-      <div className='h-[80vh] w-[100wh] hidden md:flex overflow-hidden'>
+      <div className='h-[90vh] w-[100wh] hidden md:flex overflow-hidden'>
         <div className="w-[40%] h-[80vh]">
 
         </div>
         <motion.div 
-          className="border h-[80vh] w-[60%] overflow-hidden bg-cover bg-center" 
-          style={{ backgroundImage: 'url(/burger-front2.jpg)' }}
+          className="border h-[90vh] w-[60%] overflow-hidden bg-cover bg-center" 
+          style={{ backgroundImage: 'url(/burger.png)' }}
           initial={{ opacity: 0, x: -100 }} 
           animate={{ opacity: 1, x: 0 }} 
           transition={{ duration: 1, ease: "easeOut" }}
